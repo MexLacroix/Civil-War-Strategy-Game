85e1566e1d82954e58447f61ef19cad0d06a00e8
MexLacroix <mexlacroix@gmail.com>
2023-12-26 16:57:10 -0800

    first commit

---
+++ CivilWar Strategy.py
@@ -0,0 +1,32 @@
+
+class Piece:
+    def __init__(self, name, offensive, deffensive, movement, sprite, people, )
+        self.offensive = offensive
+        self.deffensive = deffensive
+        self.movement = movement
+        self.sprite = sprite
+        self.people = people
+
+
+def Find_list(piece_list):
+    militia_num = str.piece_list.count(militia)
+    infantry_num = str.piece_list.count(infantry)
+    artillery_num = str.piece_list.count(artillery)
+    cavalry_num = str.piece_list.count(cavalry)
+    sharpshooter_num = str.piece_list.count(sharpshooter)
+    gunboat_num = str.piece_list.count(gunboat)
+
+
+    return militia_num, infantry_num, artillery_num, cavalry_num, sharpshooter_num, gunboat_num   
+    
+    
+
+def fight(attacking_peices, defending_peices):
+    print('Battle start!')
+    print(
+    
+
+
+
+    
+    
